15e9d5fd18ee0ff9c8e8989fbee5f31ed5f8f668
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-03 16:36:06 +0200

    Completamento esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-
+let onlyLetterA = (value) => { //Funzione versione arrow 
+    let letterA = [];
+    for (let i = 0; i < value.length; i++) {
+        if (value[i][0] === "A") {
+        letterA.push(value[i]);
+        }
+    }
+    console.log(letterA);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+onlyLetterA(names);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
