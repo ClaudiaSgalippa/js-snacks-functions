5db1e5cdf2ff0e4a9cb791a24ee767d7cd143f92
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-03 14:50:25 +0200

    Completamento primo esercizio

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,17 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+const saluto1 = (userName) => console.log(`Benvenuto ${userName}`);
+const saluto2 = (userName) => {
+    console.log(`Benvenuto ${userName}`);
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+saluto1(userName)
+saluto2(userName)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+//"Benvenuto Mario"
